86a991d655da2cfe7c4b9b2ebdcb7cd71dd9032b
Amadeus Wieczorek <git@amadeusw.com>
2015-06-04 02:49:51 -0400

    Built a minimal project that should repro the issue
    but it doesn't...

---
+++ src/roslynCastRepro/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roslynCastRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+                class MyClass
+                {
+                    void MyMethod()
+                    {
+                        IEnumerable<string> foo = new List<string>();
+                        var bar = foo.OfType<string>()?.FirstOrDefault()?.Length.ToString();
+
+                        MethodDeclarationSyntax method = null;
+                        var x = method?.TypeParameterList?.Parameters.ToList();
+                    }
+                }");
+
+            var mscorlib = MetadataReference.CreateFromAssembly(typeof(object).Assembly);
+            var compilation = CSharpCompilation.Create("MyCompilation",
+                syntaxTrees: new[] { tree }, references: new[] { mscorlib });
+            var model = compilation.GetSemanticModel(tree);
+
+            var rewriter = new SampleRewriter();
+            try
+            {
+                var rewrittenRoot = rewriter.Visit(tree.GetRoot());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ src/roslynCastRepro/SampleRewriter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roslynCastRepro
+{
+    class SampleRewriter : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitMemberBindingExpression(MemberBindingExpressionSyntax node)
+        {
+            Console.WriteLine("Visiting MemberBindingExpression");
+            try
+            {
+                return base.VisitMemberBindingExpression(node);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
